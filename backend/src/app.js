e127fd9b7d86be04336fe9b127586a09b2f318ab
Stiji <stijisojan2001@gmail.com>
2025-01-27 14:42:44 -0800

    stiji commit

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const signupRoute = require('./routes/signup');
 const loginRoute = require("./routes/login");
+const userRoute = require('./routes/user');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const createAdminAccount = require("./scripts/admin");
@@ -21,6 +22,7 @@ createAdminAccount();
 
 app.use('/user', signupRoute);
 app.use("/auth", loginRoute);
+app.use('/api',userRoute);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
